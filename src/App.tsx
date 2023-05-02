a2ce6b9b6212987b9d00be89cb9955cf35f9a844
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-05-02 21:12:59 +0100

    Add lesson 2 understanding Effect Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 const App = () => {
-  return <div>yo</div>;
+  const ref = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  });
+  useEffect(() => {
+    document.title = "My Apse";
+  });
+
+  return (
+    <div>
+      <label htmlFor="name" className="form-label">
+        Name
+      </label>
+      <input ref={ref} id="name" type="text" className="form-control" />
+    </div>
+  );
 };
 
 export default App;
